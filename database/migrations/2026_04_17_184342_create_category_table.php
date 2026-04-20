e7700c7d1a1a88372eb137d64754710add6f2fcd
Fernanda Bicalho Lima <fernanda-bicalho@bol.com.br>
2026-04-20 17:50:14 -0300

    Correção Campo Migration

--- database/migrations/2026_04_17_184342_create_category_table.php
+++ database/migrations/2026_04_17_184342_create_category_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('category_description')->unique();
             $table->boolean('status')->default(true);
             $table->timestamps();
